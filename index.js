ee9e1ffb12b2736f87143f565fea06794edb5ec9
Raj Chauhan <raj.chauhan000@gmail.com>
2022-11-02 16:16:47 +0000

    connected to DB

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 3000
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  })
+  mongoose.connection.on('connected', ()=>{
+    console.log('connected to DB')
+  })
+  mongoose.connection.on('error', ()=>{
+    console.log('error on connection')
+  })
 
 app.listen(PORT || process.env.PORT, ()=>{
     console.log('server is running on port: ', PORT)
