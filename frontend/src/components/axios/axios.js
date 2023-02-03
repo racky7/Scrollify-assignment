f211071e3bff3c0437e6091fb300691e76576bfd
Raj Chauhan <74532684+racky7@users.noreply.github.com>
2023-02-03 04:27:47 +0000

    backend base url changed

--- frontend/src/components/axios/axios.js
+++ frontend/src/components/axios/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: 'https://isocialmedia.herokuapp.com'});
+const instance = axios.create({baseURL: 'https://isocialmedia.onrender.com'});
 export default instance
